graysky2/profile-sync-daemon

commit ca39397bc0b7532f83c9a7a29ebd4607870dcffe

psd-resync.timer: use OnCalendar not OnUnitActiveSec
psd-resync.timer: OnUnitActiveSec was the wrong choice/the service goes
inactive after each run and OnUnitActiveSec loses the reference point.

psd-resync.service: Drop the RemainAfterExit=yes as this prevents re-
execution on every timer

Fixes #414

Default is hourly but a user drop-in can be used to override, eg:
% systemctl --user edit psd-resync.timer
 [Timer]
 # every 10 min
 OnCalendar=*:0/10

Makefile
@@ -1,4 +1,4 @@
-VERSION = 7.00
+VERSION = 7.01
 PN = profile-sync-daemon
 
 PREFIX ?= /usr

init/psd-resync.service
@@ -7,7 +7,6 @@ BindsTo=psd.service
 [Service]
 Slice=background.slice
 Type=oneshot
-RemainAfterExit=yes
 ExecStart=/usr/bin/profile-sync-daemon resync
 Environment=LAUNCHED_BY_SYSTEMD=1
 

doc/psd.1
@@ -1,5 +1,5 @@
 .\" Text automatically generated by txt2man
-.TH profile-sync-daemon 1 "10 February 2026" "" ""
+.TH profile-sync-daemon 1 "22 March 2026" "" ""
 .SH NAME
 \fBProfile-sync-daemon \fP- Symlinks and syncs browser profiles to RAM (tmpfs) thus reducing HDD/SSD calls and speeding up browsers.
 \fB
@@ -25,8 +25,6 @@ NOTE: edits made to the config file while psd is active will be applied only aft
 .IP \(bu 3
 With the release of v7.00 of psd overlayfs mode is the only supported option. It improve sync speed and offers a smaller memory footprint.
 .IP \(bu 3
-Optionally have psd resync to the filesystem prior to a sleep call. This can help in the event that the system does not properly wake up. Do this in the USE_SUSPSYNC variable.
-.IP \(bu 3
 Optionally define which browsers are to be managed in the BROWSERS array. If none are defined, the default is all detected browsers.
 .IP \(bu 3
 Optionally disable the use of crash-recovery snapshots (not recommended). Do this in the USE_BACKUPS variable.
@@ -45,7 +43,6 @@ The preview option can be called to show users exactly what psd will do/is doing
 .fam C
  systemd service: active
  resync-timer:    active
- sync on sleep:   disabled
 
  Psd will manage the following per /home/facade/.config/psd/.psd.conf:
 
@@ -76,19 +73,22 @@ The preview option can be called to show users exactly what psd will do/is doing
 .fam T
 .fi
 .SS START AND STOP PSD
-Psd ships with a systemd user service to start or stop it (psd.service). Additionally, a provided resync-timer will run an hourly resync from tmpfs back to the disk. The resync-timer is started automatically with psd.service so there is no need to start the timer; only start psd.service.
+Psd ships with a systemd user service to start or stop it (psd.service).
+.PP
+Additionally, a provided resync-timer will run an hourly resync from tmpfs back to the disk. The resync-timer is started automatically with psd.service so there is no need to start the timer; only start psd.service.
+.PP
+Of course, the frequency of the resync can be adjusted with a systemd drop-in. For example, to trigger the resync every 15 minutes:
 .PP
 .nf
 .fam C
+ % systemctl --user edit psd-resync.timer
+ [Timer]
+ OnCalendar=*:0/15
+
  % systemctl --user [option] psd.service
 
 .fam T
 .fi
-Available options:
-start
-stop
-enable
-disable
 .SS CLEAN MODE
 The clean mode will delete ALL recovery snapshots that have accumulated. Only run it when sure these are no longer needed.
 .PP

init/psd-resync.timer
@@ -3,4 +3,4 @@ Description=Timer for profile-sync-daemon - 1Hour
 BindsTo=psd.service
 
 [Timer]
-OnUnitActiveSec=1h
+OnCalendar=hourly

commit a0f605c51434e132ca0fb1183fd3a84d2659cc19

refactor(psd units): simplify resync design and fix timer activation
Reaction to breakage introduced in ca39397bc.

psd-resync.service was previously responsible for both the initial
profile sync on startup and periodic resyncs, creating conflicting
responsibilities that required RemainAfterExit=yes to keep the unit
active as a reference point for OnUnitActiveSec. This caused the timer
to silently skip re-execution since systemd considered the service
already active.

Redesign:
- psd.service owns initial sync via ExecStartPost=resync and pulls in
  the timer directly via Wants=psd-resync.timer
- psd-resync.service is now a minimal Type=oneshot stub with no
  Install section; it is an internal unit activated only by the timer
- psd-resync.timer uses OnCalendar=hourly instead of OnUnitActiveSec,
  which is wallclock-based and does not depend on service state

Only psd.service needs to be enabled.

Makefile
@@ -1,4 +1,4 @@
-VERSION = 7.01
+VERSION = 7.02
 PN = profile-sync-daemon
 
 PREFIX ?= /usr

init/psd-resync.service
@@ -1,7 +1,6 @@
 [Unit]
 Description=Timed resync
 After=psd.service
-Wants=psd-resync.timer
 BindsTo=psd.service
 
 [Service]
@@ -9,6 +8,3 @@ Slice=background.slice
 Type=oneshot
 ExecStart=/usr/bin/profile-sync-daemon resync
 Environment=LAUNCHED_BY_SYSTEMD=1
-
-[Install]
-WantedBy=default.target

init/psd.service
@@ -2,7 +2,7 @@
 Description=Profile-sync-daemon
 Documentation=man:psd(1) man:profile-sync-daemon(1)
 Documentation=https://wiki.archlinux.org/index.php/Profile-sync-daemon
-Wants=psd-resync.service
+Wants=psd-resync.timer
 RequiresMountsFor=/home/
 After=winbindd.service
 
@@ -11,6 +11,7 @@ Slice=background.slice
 Type=oneshot
 RemainAfterExit=yes
 ExecStart=/usr/bin/profile-sync-daemon startup
+ExecStartPost=/usr/bin/profile-sync-daemon resync
 ExecStop=/usr/bin/profile-sync-daemon unsync
 Environment=LAUNCHED_BY_SYSTEMD=1
 